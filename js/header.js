8897d68424ced9887ee051a2a67ae92a95861648
Jonathas Henrique <jonathasme@gmail.com>
2024-04-11 16:33:35 -0300

    Paginas Index, Contato, Formacoes e Sobre

---
+++ js/header.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('menu')
+
+menu.innerHTML = `
+<div class="box-25 text-white display-p">
+    JH Miante
+</div>
+<div class="box-75 box-75-p align-end ">
+    <a href="index.html" class="btn">Inicio</a>
+    <a href="sobre.html" class="btn">Sobre</a>
+    <a href="formacoes.html" class="btn"> Formações/Habilidades </a>
+    <a href="portfolio.html" class="btn">Portfolio</a>
+    <a href="contato.html" class="btn">Contato</a>
+</div>
+`
